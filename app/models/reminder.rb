5123dc333958c9aafca667837b9e957972fef881
Milleni Maya <milleni.maya@gmail.com>
2021-12-18 18:25:23 -0300

    More Reminder model validations and tests

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -2,8 +2,7 @@ class Reminder < ApplicationRecord
   validates :description, :start_time, :end_time, presence: {message: "Ooops, you need to provide a start time, a end time and a description" }
   validates :description, length: {  maximum: 30, too_long: "%{count} characters is the maximum allowed for descriptions" }
   validates :color, format: { with: /\A#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})\z/, message: "Not a valid color, please try again" }, allow_blank: true
-  validates :start_time, numericality: { less_then: DateTime.current }
-  validate :start_time_cannot_be_in_the_past,:end_time_cannot_be_before_start_time
+  validate :start_time_cannot_be_in_the_past,:end_time_cannot_be_before_start_time, :cannot_exist_conflicting_reminders
 
   def start_time_cannot_be_in_the_past
     if start_time.present? && start_time.to_date < Date.today
@@ -15,5 +14,20 @@ class Reminder < ApplicationRecord
     if start_time.present? && end_time.present? && end_time < start_time
       errors.add(:end_time, "can't be before start time")
     end
+  end
+
+  def cannot_exist_conflicting_reminders
+    begin
+      if start_time.present? && end_time.present? 
+        # To do: list de conflicts on the error message
+        if Reminder.where(["end_time > ? and start_time < ? and end_time >= ?", DateTime.current.beginning_of_day, end_time, start_time]).exists?
+          errors.add(:start_time, "conflicts with another reminder")
+        end
+      end
+    rescue  ActiveRecord::RecordInvalid => e
+      puts '-------------------------------------'
+      puts e
+      puts '-------------------------------------'
+    end  
   end
 end
--- spec/models/reminder_spec.rb
+++ spec/models/reminder_spec.rb
@@ -7,11 +7,12 @@ RSpec.describe Reminder, type: :model do
     it "is valid without a color" do
       expect(build_stubbed(:reminder, {color: nil})).to be_valid
     end
-    it "is valid with a description size smaller than 30 chars" do
+    it "is valid with a description size smaller or equal to 30 chars" do
+      expect(build_stubbed(:reminder, {description: "Lorem ipsum dolor sit amet con"})).to be_valid
       expect(build_stubbed(:reminder, {description: "Lorem ipsum dolor sit amet" })).to be_valid
     end
-    it "is valid with a description size equal to 30 chars" do
-      expect(build_stubbed(:reminder, {description: "Lorem ipsum dolor sit amet con"})).to be_valid
+    it "can start and end on different days as long as the start time has not passed" do
+      expect(build_stubbed(:reminder, {start_time: DateTime.current, end_time: DateTime.current + 3.day})).to be_valid
     end
     it "is not valid without a description" do
       expect(build_stubbed(:reminder, {description: nil})).to_not be_valid
@@ -33,5 +34,9 @@ RSpec.describe Reminder, type: :model do
     end
     it "is not valid with a end time before start time" do
       expect(build_stubbed(:reminder, {start_time: DateTime.current, end_time: DateTime.current - 10.minute})).to_not be_valid
+    end
+    it "is not valid with previous conflicting reminders" do
+      create(:reminder, {start_time: DateTime.current, end_time: DateTime.current + 1.hour})
+      expect(build_stubbed(:reminder, {start_time: DateTime.current + 30.minutes, end_time: DateTime.current + 2.hours})).to_not be_valid
     end
 end
